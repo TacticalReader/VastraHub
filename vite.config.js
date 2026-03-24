e587e283393f3861c285d2ea840fcf273275b41f
MR.NOBODY <126796695+TacticalReader@users.noreply.github.com>
2026-03-24 17:09:26 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-// base: '/VastraHub/' is required for GitHub Pages deployment
-export default defineConfig({
-  plugins: [react()],
-  base: '/VastraHub/',
-});
